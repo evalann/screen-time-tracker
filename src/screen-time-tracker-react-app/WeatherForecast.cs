070bbc1a93a2d3488de5702f8b4631d37cdb9782
Evalan Naidu <evalann@gmail.com>
2020-08-29 17:14:18 +0200

    Scaffolded the react and aspnetcore app.

---
+++ src/screen-time-tracker-react-app/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace screen_time_tracker_react_app
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
