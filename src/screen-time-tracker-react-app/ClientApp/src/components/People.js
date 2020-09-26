6c1c3cde91208e5cf396a7fba0b9a18bf4f9e38f
Evalan Naidu <evalann@gmail.com>
2020-09-26 11:56:34 +0200

    Set state functionality on datasheet implemented. Did some learning with the testing side and have given up until I gain better understanding of react itself.

--- src/screen-time-tracker-react-app/ClientApp/src/components/People.js
+++ src/screen-time-tracker-react-app/ClientApp/src/components/People.js
@@ -4,6 +4,7 @@ import DataSheet from 'react-datasheet';
 export class People extends Component {
     constructor(props) {
         super(props);
+        // todo: Populate from the api, if no values then show an empty grid.
         this.state = {
             peopleGrid: [
                 [{ value: "Name", readOnly: true }, { value: "Date of Birth", readOnly: true }],
@@ -15,6 +16,16 @@ export class People extends Component {
 
     onContextMenu = (e, cell, i, j) => cell.readOnly ? e.preventDefault() : null;
 
+    onCellsChanged = (changes) => {
+        const peopleGrid = this.state.peopleGrid.map(row => [...row]);
+        changes.forEach(({cell, row, col, value}) => {
+            console.log(peopleGrid[row][col]);
+            peopleGrid[row][col] = { ...peopleGrid[row][col], value };
+        })
+        console.log("got here");
+        this.setState({ peopleGrid });
+    }
+
     render() {
         return (
             <div>
@@ -24,6 +35,7 @@ export class People extends Component {
                         data={this.state.peopleGrid}
                         valueRenderer={cell => cell.value}
                         onContextMenu={this.onContextMenu}
+                        onCellsChanged={this.onCellsChanged}
                     />
                 </div>
             </div>
--- src/screen-time-tracker-react-app/ClientApp/src/components/People.test.js
+++ src/screen-time-tracker-react-app/ClientApp/src/components/People.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { People } from './People';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/dom';
 
 describe('people', () => {
     test('renders without crashing', async () => {
@@ -11,6 +13,26 @@ describe('people', () => {
     });
 
     test('expect true to be truthy', async () => {
-        expect(true).toBe(true);
+        //TODO Evalan: I'm giving up on TDD for now until I brush up on React knowledge.
+        // const { getByText, getByLabelText } = render(<People />);
+
+        // var cell = getByText('Evalan');
+
+        // fireEvent.doubleClick(cell);
+        // fireEvent.change(screen.getByRole('textbox'), {
+        //     target: { value: 'NotEvalan' },
+        //   });
+        
+        // const headingCol = getByText('Name');
+        // fireEvent.click(headingCol);
+
+        // fireEvent.blur(cell);
+
+        
+        // await new Promise(resolve => setTimeout(resolve, 2000));
+        
+
+        // cell = getByText('NotEvalan');            
+
     });
 });
